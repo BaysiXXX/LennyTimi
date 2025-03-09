d003a7b1f4ffc5c258b6affc9d22061f655ec65b
Lennard Kilian <lennardkilian@Mac.QLOCOM>
2025-03-09 17:18:50 +0100

    Fix: Entferne Submodul discord-giveaways

---
+++ LTProjekt/discord-giveaways/LTProjekt/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates // WICHTIG für DisTube!
+    ],
+    messageCacheLifetime: 60,
+    fetchAllMembers: false,
+    messageCacheMaxSize: 10,
+    shards: "auto",
+    shardCount: 5,
+    disableEveryone: true,
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+});
+
+client.commands = new Collection();
+client.queue = new Map();
+client.aliases = new Collection();
+client.cooldowns = new Collection();
+
+// Audio-Setup mit DisTube
+const { DisTube } = require("distube"); // Richtig für DisTube v4
+
+client.distube = new DisTube(client, {
+    emitNewSongOnly: true,
+});
+console.log("TOKEN:", process.env.TOKEN ? "✅ Token gefunden" : "❌ Kein Token gefunden!");
+client.login(process.env.TOKEN);
---
+++ LTProjekt/discord-giveaways/README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# LennyTimi
+=======
+# discord-giveaways
+A complete framework to facilitate the creation of giveaways using discord.js v12
+>>>>>>> ac8c9c7 (Initial commit)
